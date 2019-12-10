f763d2a445c84a993a0b6134c2dfbc8f947f7598
SEJRANI Besjan <Besjan.SEJRANI@cpnv.ch>
2019-12-10 14:17:03 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+=======================================================================
+| 1. Title Bataille navale
+|
+| 2. Description: 
+|    Jouer a la bataille navale alors que la position
+|    des bateaux est codée dans le programme
+|
+| 3. Author: Besjan Sejrani
+|
+| 4. Version: 0.1
+|
+| 5. Date: 10.12.19
+=======================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world");
+    return 0;
+}
